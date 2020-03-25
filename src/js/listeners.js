c5efab9667c7beeb25a7c10d2c81988a476c1934
Alexeii Yatsenko <alexeii.yatsenko@gmail.com>
2020-03-25 16:31:47 +0200

    deleted some useless strokes

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,52 +1,13 @@
 'use strict';
 
 function windowHandler() {
-    const data = {
-        id: Math.random(),
-        title: 'Custom Laptop Limited',
-        price: '1000',
-        iconPath: 'https://picsum.photos/200',
-        imgPath: 'https://i.picsum.photos/id/924/200/300.jpg',
-        characteristics: {
-            cpu: 'Intel I-5',
-            ram: '8Gb',
-            hd: '120Gb'
-        }
-    };
-    
-    const myProduct = [
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data),
-        new Product(data)
-    ];
-
-    new ProductsList(myProduct).init();
+    new ProductsList().init();
 }
 
 function productsHandler() {
-    const data = {
-        id: Math.random(),
-        title: 'Custom Laptop Limited',
-        price: '1000',
-        iconPath: 'https://picsum.photos/200',
-        imgPath: 'https://i.picsum.photos/id/924/200/300.jpg',
-        characteristics: {
-            cpu: 'Intel I-5',
-            ram: '8Gb',
-            hd: '120Gb'
-        }
-    };
-
     if (
         event.target.parentElement.classList.contains('product') ||
         event.target.classList.contains('product')) {
-            console.log(new Product(data).expandedView());
+            console.log(new Product().expandedView());
     }
 }
