79d08c85fc9de10f7d48ee9f523de19d4df334c2
MichaelNeikovych <robbery06091991@gmail.com>
2020-03-25 12:39:55 +0200

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', windowHandler);
+
+$('#modal-window-btn').addEventListener('click', modalWindowOpenHandler);
+
+$('#close-btn').addEventListener('click', modalWindowCloseHandler);
--- src/js/listeners.js
+++ src/js/listeners.js
@@ -3,3 +3,14 @@
 function windowHandler() {
 
 }
+
+function modalWindowOpenHandler(){
+    $('body').classList.add('body-modal-window');
+    $('#modal').innerText = '';
+    new Modal();
+}
+
+function modalWindowCloseHandler(){
+    $('body').classList.remove('body-modal-window');
+    $('#modal').classList.add('modal-window__none');
+}
