0fa21a0158b653438dd8ae64a893b01882e5ff54
MichaelNeikovych <robbery06091991@gmail.com>
2020-03-31 14:38:52 +0300

    third commit

--- src/js/entities/Filter.js
+++ src/js/entities/Filter.js
@@ -7,38 +7,31 @@ class Filter extends Component {
 
     drawFilter(category){
         for(let key in category){
-            console.log(key);
             const filterBox = new Component($nR('#filter'));
             filterBox.addClass('filter-category-wrap');
             
             for(let i = 0; i < category[key].length; i++){
                 const characteristics = category[key][i].characteristics;
-                // console.log(characteristics)
+                console.log(category[key][i].id)
+
 
                 for(let elem in characteristics){
-                    console.log(`${elem}: ${characteristics[elem]}`)
+                    console.log(`${elem}: ${characteristics[elem]}`);
+
+                    const filterItemHead = new Element('h3', filterBox);
+                    filterItemHead.addClass('filter-item__head');
+                    filterItemHead.html(`${capitalize(elem)}`);
+
+                    
+                    const filterItemList = new Element('ul', filterBox);
+                    filterItemList.addClass('filter-item__list');
+
+
+                    const filterItemValue = new Element('li', filterItemList);
+                    filterItemValue.addClass('filter-item__value');
+                    filterItemValue.html(`${capitalize(characteristics[elem])}`);
+                    filterItemValue.attr({'data-id' : category[key][i].id});
                 }
-            }
-        }
-
-
-        for(let i = 0; i < array.length; i++){
-            const filterBox = new Component($nR('#filter'));
-            filterBox.addClass('filter-category-wrap');
-
-            for(let key in array[i]){
-                const filterItemHead = new Element('h3', filterBox);
-                filterItemHead.addClass('filter-item__head');
-                filterItemHead.html(`${capitalize(key)}`);
-
-                
-                const filterItemList = new Element('ul', filterBox);
-                filterItemList.addClass('filter-item__list');
-
-
-                const filterItemValue = new Element('li', filterItemList);
-                filterItemValue.addClass('filter-item__value');
-                filterItemValue.html(`${capitalize(array[i][key])}`);
             }
         }
     }
