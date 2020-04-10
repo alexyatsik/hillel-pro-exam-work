07b77f1974dfd6834e58edf5b0c85f3085603a6c
MichaelNeikovych <robbery06091991@gmail.com>
2020-04-10 17:08:21 +0300

    Got array of filtred objects with toggle effect

--- src/js/entities/Filter.js
+++ src/js/entities/Filter.js
@@ -1,17 +1,18 @@
 'use strict';
 
 class Filter extends Component {
-    constructor(category){
+    constructor(category) {
         super();
         const filterObject = this.getFilterObject(category);
         this.drawFilters(filterObject);
         this.getProductsFromFilter(category);
-    } 
+        // console.log(test)
+    }
 
-    getFilterObject(currentCategory){
+    getFilterObject(currentCategory) {
         const filtersCollection = {};
         const filtersCollectionKey = new Set();
-        
+
         currentCategory.forEach((elem) => {
             Object.keys(elem.characteristics).forEach((key) => {
                 filtersCollectionKey.add(key);
@@ -24,27 +25,27 @@ class Filter extends Component {
 
         currentCategory.forEach((elem) => {
             filtersCollectionKey.forEach((keyMap) => {
-                if(elem.characteristics[keyMap]){
+                if (elem.characteristics[keyMap]) {
                     filtersCollection[keyMap].add(elem.characteristics[keyMap]);
                 }
             })
         })
-       
+
         return filtersCollection;
     }
 
-    drawFilters(filterObj){
+    drawFilters(filterObj) {
         const filterBox = new Component($nR('#filter'));
         filterBox.addClass('filter-category-wrap');
 
-        for(let filterName in filterObj){
+        for (let filterName in filterObj) {
             const filterItemHead = new Element('h3', filterBox);
             filterItemHead.addClass('filter-item__head');
             filterItemHead.html(`${capitalize(filterName)}`);
             const filterItemList = new Element('ul', filterBox);
             filterItemList.addClass('filter-item__list');
-            
-            for(let filterValue of filterObj[filterName]){
+
+            for (let filterValue of filterObj[filterName]) {
                 const filterItemValue = new Element('li', filterItemList);
                 filterItemValue.addClass('filter-item__value');
                 filterItemValue.html(`${capitalize(filterValue)}`);
@@ -53,8 +54,9 @@ class Filter extends Component {
     }
 
 
-    getProductsFromFilter(currentCategory){
+    getProductsFromFilter(currentCategory) {
         const filtredProductsCollection = new Set();
+        let filtredProductsArray;
 
         let itemList = document.querySelectorAll('.filter-item__list');
 
@@ -65,14 +67,19 @@ class Filter extends Component {
 
                 currentCategory.forEach(obj => {
                     let object = obj;
-                    for(let key in object.characteristics){
-                        if(target === object.characteristics[key]){
+                    for (let key in object.characteristics) {
+                        if (target === object.characteristics[key] && !filtredProductsCollection.has(object)) {
                             filtredProductsCollection.add(object);
+                            filtredProductsArray = Array.from(filtredProductsCollection);
+                        } else if (target === object.characteristics[key] && filtredProductsCollection.has(object)) {
+                            filtredProductsCollection.delete(object);
+                            filtredProductsArray = Array.from(filtredProductsCollection);
                         }
+                        // return filtredProductsArray;
                     }
                 })
-                console.log(filtredProductsCollection);
-        })});
+                console.log(filtredProductsArray)
+            })
+        });
     }
-
 }
